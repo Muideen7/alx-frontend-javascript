37e4408fbd2cf100c26574826e4b5402c1d70bfd
muideen7 <olayeyeayomide2000@gmail.com>
2023-06-26 22:25:20 +0100

    2-arrow.js

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,8 @@
+export default function getNeighborhoodsList() {
+	this.sanFransciscoNeighborhoods = ['SOMA', Union square'];
+
+	this.addNeighborhood = (newNeighborhood) => {
+		this.sanFranscicoNeighborhoods.push(newNeighborhood);
+		return this.sanFranscicoNeighborhoods;
+	};
+}
