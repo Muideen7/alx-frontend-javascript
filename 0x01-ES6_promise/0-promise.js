3773e71f630c65c8fe6a771436e097fd81d649a1
muideen7 <olayeyeayomide2000@gmail.com>
2023-06-29 19:08:25 +0100

    0-promise.js

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Perform some asynchronous task, like making an API call
+    // When the task is complete, call resolve with the desired result
+    // If there's an error, call reject with the error message
+  });
+}
