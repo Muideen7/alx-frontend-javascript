2a3e5948570475aac11c09e2843800fc0c3060db
muideen7 <olayeyeayomide2000@gmail.com>
2023-07-01 18:41:12 +0100

    7-airport.js

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,11 @@
+/*eslint-disable */
+export default class Airport {
+    constructor (name, code) {
+        this._name = name;
+        this._code = code;
+    }
+
+    get [Symbol.toStringTag] () {
+        return this._code;
+    }
+}
