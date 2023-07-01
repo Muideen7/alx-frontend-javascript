1c875b72191d4386d19b1fa35bb0958f53bdc301
muideen7 <olayeyeayomide2000@gmail.com>
2023-07-01 18:39:42 +0100

    5-building.js

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,14 @@
+/*eslint-disable */
+export default class Building {
+    constructor (sqft){
+        if (this.constructor !== Building && typeof(this.evacuationWarningMessage) !== 'function') {
+            throw new Error('Class extending Building must override evacuationWarningMessage');
+        }
+        this._sqft = sqft;
+    }
+
+    get sqft () {
+        return this._sqft;
+    }
+
+}
