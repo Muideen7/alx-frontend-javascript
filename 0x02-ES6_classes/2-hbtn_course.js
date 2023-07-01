0150bd78cdcaddb9d161789f1c081022735202b5
muideen7 <olayeyeayomide2000@gmail.com>
2023-07-01 18:33:35 +0100

    2-hbtn_course.js

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,47 @@
+/*eslint-disable */
+export default class HolbertonCourse {
+    constructor (name, length, students) {
+        if (typeof(name) !== 'string'){
+            throw new Error('Name must be a string');
+        }
+        else if (typeof(length) !== 'number'){
+            throw new Error('Length must be a number');
+        }else if (!Array.isArray(students)){
+            throw new Error('Students must be an Array')
+        }
+
+        this._name = name;
+        this._length = length;
+        this._students = students;
+    }
+    get name(){
+        return this._name;
+    }
+    get length() {
+        return this._length;
+    }
+    get students() {
+        return this._students;
+    }
+
+    set name(name) {
+        if (typeof name !== 'string') {
+            throw new TypeError('Name must be a string');
+          }
+          this._name = name;
+    }
+
+    set length(length) {
+        if (typeof length !== 'number') {
+            throw new TypeError('Name must be a string');
+          }
+          this._length = length;
+    }
+
+    set students(students) {
+        if (!Array.isArray(students)) {
+          throw new TypeError('Students must be an array of strings');
+        }
+        this._students = students;
+      }
+}
